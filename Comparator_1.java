4c9e7463c12ae26e67b4e224b80db1914709d4a3
omaradel01 <36343846+omaradel01@users.noreply.github.com>
2020-03-05 20:04:42 +0200

    Create Comparator_1.java

---
+++ Comparator_1.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class Comparator_1 implements Comparator<Pair<Record,Integer>> {
+
+	@Override
+	public int compare(Pair<Record, Integer> record, Pair<Record, Integer> another_record) {
+		return Integer.compare(record.Get_T1().getKey(), another_record.Get_T1().getKey());
+	}
+
+}
