fd78077556c469f229b0da683119974160279c94
omaradel01 <36343846+omaradel01@users.noreply.github.com>
2020-03-09 11:47:42 +0200

    Delete MyComparator.java

--- MyComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class MyComparator implements Comparator<Record> {
-
-	@Override
-	public int compare(Record r1, Record r2) {
-		return Integer.compare(r1.getKey(), r2.getKey());
-	}
-}
